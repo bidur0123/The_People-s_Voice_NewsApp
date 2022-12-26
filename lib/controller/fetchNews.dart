d197a234b200b752c6116f0197c8d7ffadd8fedb
Bidur Gupta <bidurgupta125@gmail.com>
2022-12-27 00:51:53 +0530

    second commit

--- lib/controller/fetchNews.dart
+++ lib/controller/fetchNews.dart
@@ -52,7 +52,7 @@ class FetchNews {
     var sourceID = sourcesId[_random.nextInt(sourcesId.length)];
 
     Response response = await get(Uri.parse(
-      //  "https://newsapi.org/v2/top-headlines?sources=$sourceID&apiKey=caea254bf2f94f869e831ec24284ece1"));
+     //   "https://newsapi.org/v2/top-headlines?sources=$sourceID&apiKey=caea254bf2f94f869e831ec24284ece1"));
         "https://newsapi.org/v2/top-headlines?sources=$sourceID&apiKey=c96fd315e7a7464ab6b329ecf409bb86"));
     Map body_data = jsonDecode(response.body);
     List articles = body_data["articles"];
